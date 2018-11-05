90436beadfa44b3befcffdd3690f9ef69265ad07
Mikko Maja <mikmaj88@gmail.com>
2018-11-05 21:15:29 +0200

    Add password hashing on registering

--- flasksite/__init__.py
+++ flasksite/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f829d4819f5f2db8c7fc71337c6d62fe'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flasksite import routes
