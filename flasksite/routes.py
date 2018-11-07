ebf13860166a1bd348ba2add8c66bcbf0aa0206a
Mikko Maja <mikmaj88@gmail.com>
2018-11-07 14:51:53 +0200

    Add new post form

--- flasksite/routes.py
+++ flasksite/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flasksite import app, db, bcrypt
-from flasksite.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flasksite.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flasksite.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -107,3 +107,13 @@ def account():
     image_file = url_for(
         'static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
